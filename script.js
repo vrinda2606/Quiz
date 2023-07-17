799ac925dfc05215a2ccb89798cc53a30c89aa57
Vrinda Sharma <86602126+vrinda2606@users.noreply.github.com>
2023-07-18 03:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const exitButton = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueButton= document.querySelector('.continue-btn');
 const quiz = document.querySelector('.quiz-section');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startButton.onclick = () => {
     popup.classList.add('active');
@@ -17,4 +19,28 @@ continueButton.onclick= () => {
     quiz.classList.add('active');
     popup.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQues(0);
+}
+
+
+
+let  quesCnt = 0;
+const  nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick= () => {
+    quesCnt++;
+    showQues(quesCnt);
+}
+
+const quizOptions=document.querySelector('.quiz-options');
+
+//ques from array with options
+
+function showQues(index){
+    const quesText = document.querySelector('.ques-text');
+    quesText.textContent = `${questions[index].num}. ${questions[index].question}`;
+
+    let optionTag = `<div class="options"><span>${questions[index].options[0]}</span></div>`;
 }
