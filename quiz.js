762ec51d0d7419187d32d0b283a754ab0699df9a
Vrinda Sharma <86602126+vrinda2606@users.noreply.github.com>
2023-07-18 17:38:24 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,111 +2,111 @@ let questions = [
     {
         num: 1, 
         question: "Which attribute is used to specify multiple alternative sources for an image?",
-        answer:"B.srcset",
+        answer:"B. srcset",
         options:[
-            "A.alt",
-            "B.srcset",
-            "C.multiple",
-            "D.media"
+            "A. alt",
+            "B. srcset",
+            "C. multiple",
+            "D. media"
         ]
     },
     {
         num: 2, 
         question: "Which attribute is used to define a keyboard shortcut for an element?",
-        answer:"",
+        answer:"C. accesskey",
         options:[
-            "A.shortcut",
-            "B.accelerator",
-            "C.accesskey",
-            "D.keyshort"
+            "A. shortcut",
+            "B. accelerator",
+            "C. accesskey",
+            "D. keyshort"
         ]
     },
     {
         num: 3, 
         question: "Which HTML5 attribute is used to specify that the input field must be filled out before submitting a form?",
-        answer:"A.required",
+        answer:"A. required",
         options:[
-            "A.required",
-            "B.validate",
-            "C.mandatory",
-            "D.validate-required"
+            "A. required",
+            "B. validate",
+            "C. mandatory",
+            "D. validate-required"
         ]
     },
     {
         num: 4, 
         question: "Which CSS property is used to create an animated transition effect between different property values?",
-        answer:"A.transition",
+        answer:"A. transition",
         options:[
-            "A.transition",
-            "B.transform",
-            "C.animation",
-            "D.opacity"
+            "A. transition",
+            "B. transform",
+            "C. animation",
+            "D. opacity"
         ]
     },
     {
         num: 5, 
         question: "Which CSS selector has the highest specificity",
-        answer:"B.ID selector",
+        answer:"B. ID selector",
         options:[
-            "A.Class selector",
-            "B.ID selector",
-            "C.Attribute selector",
-            "D.Universal selector"
+            "A. Class selector",
+            "B. ID selector",
+            "C. Attribute selector",
+            "D. Universal selector"
         ]
     },
     {
         num: 6, 
         question: "Which CSS property is used to control the stacking order of elements on a webpage?",
-        answer:"A.z-index",
+        answer:"A. z-index",
         options:[
-            "A.z-index",
-            "B.position",
-            "C.float",
-            "D.display"
+            "A. z-index",
+            "B. position",
+            "C. float",
+            "D. display"
         ]
     },
     {
         num: 7, 
         question: "What is a closure in JavaScript?",
-        answer:"B.A function that has access to variables from its outer function scope even after the outer function has returned.",
+        answer:"B. A function that has access to variables from its outer function scope even after the outer function has returned.",
         options:[
-            "A.A built-in object in JavaScript used for data encapsulation.",
-            "B.A function that has access to variables from its outer function scope even after the outer function has returned.",
-            "C.A JavaScript file that contains reusable code.",
-            "D.A way to create private variables in JavaScript."
+            "A. A built-in object in JavaScript used for data encapsulation.",
+            "B. A function that has access to variables from its outer function scope even after the outer function has returned.",
+            "C. A JavaScript file that contains reusable code.",
+            "D. A way to create private variables in JavaScript."
         ]
     },
     {
         num: 8, 
         question: "What is the difference between the '==' and '===' operators in JavaScript?",
-        answer:"B.'==' compares values, while '===' compares values and types.",
+        answer:"B. '==' compares values, while '===' compares values and types.",
         options:[
-            "A.'==' compares values and types, while '===' only compares values.",
-            "B.'==' compares values, while '===' compares values and types.",
-            "C.'==' performs type coercion, while '===' does not perform type coercion.",
-            "D.'==' is used for strict equality, while '===' is used for loose equality."
+            "A. '==' compares values and types, while '===' only compares values.",
+            "B. '==' compares values, while '===' compares values and types.",
+            "C. '==' performs type coercion, while '===' does not perform type coercion.",
+            "D. '==' is used for strict equality, while '===' is used for loose equality."
         ]
     },
     {
         num: 9, 
         question: "What is the purpose of using the \"use strict\" directive at the beginning of a JavaScript file or a function?",
-        answer:"A.It enforces stricter error handling and helps prevent common coding mistakes.",
+        answer:"A. It enforces stricter error handling and helps prevent common coding mistakes.",
         options:[
-            "A.It enforces stricter error handling and helps prevent common coding mistakes.",
-            "B.It enables the use of modern JavaScript features and syntax.",
-            "C.It restricts the usage of global variables within the script.",
-            "D.It allows the use of strict mode for debugging purposes."
+            "A. It enforces stricter error handling and helps prevent common coding mistakes.",
+            "B. It enables the use of modern JavaScript features and syntax.",
+            "C. It restricts the usage of global variables within the script.",
+            "D. It allows the use of strict mode for debugging purposes."
         ]
     },
     {
         num: 10, 
         question: "What is the difference between \"undefined\" and \"null\" in JavaScript?",
-        answer:"B.\"undefined\" means a variable has been declared but has not been assigned a value, while \"null\" is an assigned value that represents no value or no object.",
+        answer:"B. \"undefined\" means a variable has been declared but has not been assigned a value, while \"null\" is an assigned value that represents no value or no object.",
         options:[
-            "A.\"undefined\" is a primitive value, while \"null\" is an object.",
-            "B.\"undefined\" means a variable has been declared but has not been assigned a value, while \"null\" is an assigned value that represents no value or no object.",
-            "C.\"undefined\" is a global object, while \"null\" is a local object.",
-            "D.\"undefined\" is used to define null values, while \"null\" is used for undefined values."
+            "A. \"undefined\" is a primitive value, while \"null\" is an object.",
+            "B. \"undefined\" means a variable has been declared but has not been assigned a value, while \"null\" is an assigned value that represents no value or no object.",
+            "C. \"undefined\" is a global object, while \"null\" is a local object.",
+            "D. \"undefined\" is used to define null values, while \"null\" is used for undefined values."
         ]
     }
 ];
